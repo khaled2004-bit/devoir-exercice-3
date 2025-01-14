7ce39f0075a503df3faf4c7682b72c825535acc3
khaled2004-bit <afazaz.khalid@etu.uae.ac.ma>
2025-01-14 18:13:02 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct Call {
+    int callID;
+    char status;
+    struct Call* next;
+    struct Call* prev;
+} Call;
+
+
+Call* createCall(int callID, const char* status) {
+    Call* newCall = (Call*)malloc(sizeof(Call));
+    newCall->callID = callID;
+    newCall->next = NULL;
+    newCall->prev = NULL;
+    return newCall;
+}
+
+void addCall(Call** head, int callID, const char* status) {
+    Call* newCall = createCall(callID, status);
+
+    if (*head == NULL) {
+        *head = newCall;
+    } else {
+        Call* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newCall;
+        newCall->prev = temp;
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
